39ff309f01ebf591d6dad8ada65576197e14b85b
Soham Suryawanshi <119783385+OneTeraByte7@users.noreply.github.com>
2024-05-19 20:35:19 +0530

    Add files via upload

---
+++ Python Control Flow/Print Evens.py
@@ -0,0 +1,11 @@
+"""Question : Write a program that prints the first 20 even numbers. There are several correct approaches, 
+              but they all use a loop of some sort. Do no write twenty print statements :-)
+"""
+
+def main():
+    for i in range(20):
+        print(i * 2)  
+   
+
+if __name__ == "__main__":
+    main()
