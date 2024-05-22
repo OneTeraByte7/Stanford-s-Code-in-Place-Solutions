0502c45e4b056405d0345bd0616822fbbc580b0b
Soham Suryawanshi <119783385+OneTeraByte7@users.noreply.github.com>
2024-05-22 22:02:49 +0530

    Create International Voting age.py

---
+++ Python Control Flow/International Voting age.py
@@ -0,0 +1,27 @@
+"""Question : Write a program which asks a user for their age and lets them know if they can or can't vote in the following three fictional countries.
+"""
+
+PETURKSBOUIPO_AGE = 16
+STANLAU_AGE = 25
+MAYENGUA_AGE = 48
+
+def main():
+    user_age = int(input("How old are you? "))
+
+    if user_age >= PETURKSBOUIPO_AGE:
+        print("You can vote in Peturksbouipo where the voting age is " + str(PETURKSBOUIPO_AGE) + ".")
+    else:
+        print("You cannot vote in Peturksbouipo where the voting age is " + str(PETURKSBOUIPO_AGE) + ".")
+    
+    if user_age >= STANLAU_AGE:
+        print("You can vote in Stanlau where the voting age is " + str(STANLAU_AGE) + ".")
+    else:
+        print("You cannot vote in Stanlau where the voting age is " + str(STANLAU_AGE) + ".")
+
+    if user_age >= MAYENGUA_AGE:
+        print("You can vote in Mayengua where the voting age is " + str(MAYENGUA_AGE) + ".")
+    else:
+        print("You cannot vote in Mayengua where the voting age is " + str(MAYENGUA_AGE) + ".")
+
+if __name__ == '__main__':
+    main()
