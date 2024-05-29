55794d87e2db0d77b9fa6d57d47877b9772dd2d2
Soham Suryawanshi <119783385+OneTeraByte7@users.noreply.github.com>
2024-05-29 22:07:07 +0530

    Add files via upload

---
+++ Diagnostic Test Problem's Solution/print even odd.py
@@ -0,0 +1,17 @@
+"""Question : Print out each of the numbers 1 through 100 and whether that number is even or odd.
+              100 is specified using a constant MAX_NUMBER.Here is what the output looks like when 
+              MAX_NUMBER = 100
+"""
+
+MAX_NUMBER = 100
+
+def main():
+    for i in range(1,101):
+        if i % 2 == 0:
+            print(i,"is even")
+        else:
+            print(i,"is odd")
+    
+
+if __name__ == "__main__":
+    main()
